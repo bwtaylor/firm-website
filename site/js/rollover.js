c9d61f2da77954c83542539f4373081a9be31211
Bryan Taylor <btaylor@rackspace.com>
2012-04-14 15:54:09 -0500

    initial checkin of basic site

---
+++ site/js/rollover.js
@@ -0,0 +1,12 @@
+
+
+function SwapOut() {
+  document.contact.src = "images/contact-x-button.png"
+  return true;
+} 
+
+function SwapBack(){
+  document.contact.src = "images/contact-button.png"
+  return true;
+}
+
